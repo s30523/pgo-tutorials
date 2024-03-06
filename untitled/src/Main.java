f716f8c015388904d5dcc4673f7c47d88d2bb729
Kaja Ambroziak <s30523@pjwstk.edu.pl>
2024-03-06 13:31:31 +0100

    Exercise 1

---
+++ untitled/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+
+        if(a >0)
+
+    {
+        System.out.println("The number is positive");
+    } else if(a< 0)
+
+    {
+        System.out.println("The number is negative");
+    } else if(a ==0)
+
+    {
+        System.out.println("The value equals 0");
+    }
+
+}
+    }
+
